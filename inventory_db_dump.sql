ca1ed7a6bb93bbdbef446378de3d010171846b5d
Jikun Li <lijikun15@gmail.com>
2026-04-28 13:16:10 +0100

    add mysqldump of inventory_db with sample data

---
+++ inventory_db_dump.sql
@@ -0,0 +1,101 @@
+CREATE DATABASE  IF NOT EXISTS `inventory_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `inventory_db`;
+-- MySQL dump 10.13  Distrib 8.0.46, for Win64 (x86_64)
+--
+-- Host: localhost    Database: inventory_db
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `appliances`
+--
+
+DROP TABLE IF EXISTS `appliances`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `appliances` (
+  `appliance_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `appliance_type` varchar(50) NOT NULL,
+  `brand` varchar(50) NOT NULL,
+  `model_number` varchar(50) NOT NULL,
+  `serial_number` varchar(50) NOT NULL,
+  `purchase_date` date NOT NULL,
+  `warranty_expiration_date` date NOT NULL,
+  `cost` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`appliance_id`),
+  UNIQUE KEY `serial_number` (`serial_number`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `appliances_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appliances`
+--
+
+LOCK TABLES `appliances` WRITE;
+/*!40000 ALTER TABLE `appliances` DISABLE KEYS */;
+INSERT INTO `appliances` VALUES (4,3,'Washing Machine','Swan','DW3000','DW-5567-X2','2026-04-28','2028-04-28',888.00);
+/*!40000 ALTER TABLE `appliances` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `eircode` varchar(8) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'mike','jorden','Griffith college','+353 87 0348888','lijikun15@qq.com','D08 VK00'),(2,'luck','good','Griffith college','+353 87 8888888','luckgood@qq.com','D08 1234'),(3,'good','luck','Griffith College','+353 88 8888888','goodluck@gmail.com','D08 8888');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'inventory_db'
+--
+
+--
+-- Dumping routines for database 'inventory_db'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-28 12:45:22
